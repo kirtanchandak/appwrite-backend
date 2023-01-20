7cc2e17423a2d223d2846b8d2a768e35564d4875
kirtanchandak <themarketingkirtan@gmail.com>
2023-01-20 22:33:47 +0530

    folder structure and routing done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
-  return <div className="App">Kirtan</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
